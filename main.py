f9023dd02d99d1889e4032995e3702cd1cad2fca
queenanna1999 <97907559+queenanna1999@users.noreply.github.com>
2022-04-01 15:43:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -796,11 +796,19 @@ async def on_message(message):
        
     if "아가리" in message.content:
         await message.delete()
-        await message.channel.send(f"{message.author.mention} 님이 금칙어를 사용하였습니다.")   
+        await message.channel.send(f"{message.author.mention} 님이 금칙어를 사용하였습니다.") 
+       
+    if "ㅈㄹ" in message.content:
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} 님이 사용하신 단어는 간질병환자들을 조롱하는 의미를 가진 욕설이고, 우리 서버 규칙을 어기는 행위죠.")        
        
     if "보빨" in message.content:
         await message.delete()
         await message.channel.send(f"{message.author.mention} 님, 일,베 또는 디,시와 같은 남초 커뮤니티에서 파생된 단어를 사용하지 마세요.") 
+       
+    if "노무" in message.content:
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} 님, 일,베 또는 디,시와 같은 남초 커뮤니티에서 파생된 단어를 사용하지 마세요.")        
        
     if "보1빨" in message.content:
         await message.delete()
